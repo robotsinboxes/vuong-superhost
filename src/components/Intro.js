5798e7d5d1f104a9f564f9e10e8a8c6a68f0cadf
Lisa Goodell <robotsinboxes@gmail.com>
2022-07-20 18:32:30 -0500

    Styling Intro section

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,23 +1,27 @@
 import './Styles.scss';
-
+//import { TbBrandAirbnb } from 'react-icons/tb'
 import AirbnbPng from '../images/airbnb.png';
 
 const Intro = () => {
    return (
       <div className="intro-container">
-         <div className="intro-name">
+         <div className='circle'></div>
+         <div className="intro">
             <p>Learn tips</p>
-            <p>from an official</p>
+            <p>from an official</p> 
+            {/* <p className='airbnb'>
+               <span className='airbnb-icon'><TbBrandAirbnb/></span>
+               airbnb
+            </p> */}
          </div>
          <div className="intro-airbnb">
-            {/* <h1><FaAirbnb/> Airbnb</h1> */}
             <img className='airbnb-png' src={AirbnbPng} alt='airbnb'></img>
          </div>
          <div className="intro-super">
-            <h1>Superhost</h1>
+            <h1>SUPERHOST</h1>
          </div>
          <div className="intro-ambass">
-            <h1>Ambassador</h1>
+            <h1>AMBASSADOR</h1>
          </div>
       </div>
    )
