ce37f5bbab60c78a947664f3d3571306f87d18e9
Lisa Goodell <robotsinboxes@gmail.com>
2022-08-02 20:32:40 -0500

    Finished MVP for demo - waiting for feedback

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -5,20 +5,22 @@ import AirbnbPng from '../images/airbnb.png';
 const Intro = () => {
    return (
       <div className="intro-container">
+         {/* <div className="circle"></div> */}
          <div className="intro-bg-img">
             <img src={LivingRoom} alt='airbnb'></img>
-         </div>
-         <div className='circle'></div>
-         <div className="intro">
-            <p>Learn tips</p>
-            <p>from an official</p> 
          </div>
          <div className="intro-airbnb">
             <img className='airbnb-png' src={AirbnbPng} alt='airbnb'></img>
          </div>
          <div className="intro-title">
-            <h1 className='super'>SUPERHOST</h1>
-            <h1 className='ambass'>AMBASSADOR</h1>
+            <div className="intro">
+               <p>Learn tips</p>
+               <p>from an official</p> 
+            </div>
+            <div className="title">
+               <h1 className='super'>SUPERHOST</h1>
+               <h1 className='ambass'>AMBASSADOR</h1>
+            </div>
          </div>
          
          {/* ARROW test - @Jakub Honisek, Codepen*/}
--- src/components/Services.js
+++ src/components/Services.js
@@ -1,5 +1,6 @@
 import './Styles.scss';
 import Bedroom from '../images/bedroom.jpg';
+import Bathroom from '../images/bathroom.jpg';
 
 const Services = () => {   
    return (
@@ -9,25 +10,26 @@ const Services = () => {
             <h1>What Can I Help You With?</h1>
             <hr></hr>
             <p>Transitioning from Airbnb travelers to hosts was easy for us because we were such frequent travelers, and learned a lot from the places we stayed. (It also helped that we had a genuine love and knack for decorating and hosting). But for many people - the thought of inviting strangers into your house can be nerve-wracking. I can help alleviate some of those worries, and walk you through booking, vetting, security, insurance, payouts, and much more.</p>
-            <br></br>
-            <p>As a Superhost, I can also help give you best practices for furnishing and designing the experience. I can help you ‘sell’ your space with guidance around photography, descriptions, and amenities. Finally, I’ll help you get the place ready to welcome your first guests.</p>
-            <br></br>
-            <p className='link'>To get started, just click on 
-               <a href='https://www.airbnb.com/askasuperhost?from=mentee_landing_page&ambassadorUserId=14564141&r=teddyvuong&s=9' target='_blank' rel='noreferrer' className='link'> this link</a>
-            , and Airbnb will connect you with me. Send me a quick introduction and i’ll get back to you the same day. </p>
-         
+        
          </div>
          <div className="bedroom-img"> 
             <img src={Bedroom} alt="bedroom" />
+         </div>
+         <div className="services-text-pp2">
+            <p className='link'>To get started, just click on 
+               <a href='https://www.airbnb.com/askasuperhost?from=mentee_landing_page&ambassadorUserId=14564141&r=teddyvuong&s=9' target='_blank' rel='noreferrer' className='link'> this link</a>
+            , and Airbnb will connect you with me. Send me a quick introduction and i’ll get back to you the same day. </p>
+            <br></br>
+            <p>As a Superhost, I can also help give you best practices for furnishing and designing the experience. I can help you ‘sell’ your space with guidance around photography, descriptions, and amenities. Finally, I’ll help you get the place ready to welcome your first guests.</p>
          </div>
          <div className="button-container">
             <a href='https://www.airbnb.com/askasuperhost?from=mentee_landing_page&ambassadorUserId=14564141&r=teddyvuong&s=9' target='_blank' rel='noreferrer' className='btn'>
                Get started!
             </a>
          </div>
-         {/* <div className="livingrm-img"> 
+         <div className="bathroom-img"> 
             <img src={Bathroom} alt="bathroom" />
-         </div> */}
+         </div>
       </div>
    )
 };
